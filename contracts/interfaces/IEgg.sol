529bf8252053cf5b0db396e170244755c5d1534c
NicoBarragan <nbarragan01@gmail.com>
2022-08-30 01:13:37 -0300

    feat(IEgg.sol): added burn function and OnlyAnts error

--- contracts/interfaces/IEgg.sol
+++ contracts/interfaces/IEgg.sol
@@ -5,4 +5,8 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 interface IEgg is IERC20 {
   function mint(address, uint256) external;
+
+  function burn(address, uint256) external;
+
+  error OnlyAnts(address ants, address badSender);
 }
