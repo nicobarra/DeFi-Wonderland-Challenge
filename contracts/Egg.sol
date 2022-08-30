e952e32c007da290f6a5b87e14e8c3771692c8cc
NicoBarragan <nbarragan01@gmail.com>
2022-08-30 01:15:25 -0300

    feat(Egg.sol): added burn func and used a modifier OnlyAnts for assertion in mint and burn funcs

--- contracts/Egg.sol
+++ contracts/Egg.sol
@@ -7,14 +7,22 @@ import './interfaces/IEgg.sol';
 contract Egg is ERC20, IEgg {
   address private _ants;
 
+  modifier onyAnts() {
+    if (msg.sender != _ants) revert OnlyAnts(_ants, msg.sender);
+    _;
+  }
+
   constructor(address __ants) ERC20('EGG', 'EGG') {
     _ants = __ants;
   }
 
-  function mint(address _to, uint256 _amount) external override {
-    //solhint-disable-next-line
-    require(msg.sender == _ants, 'Only the ants contract can call this function, please refer to the ants contract');
+  function mint(address _to, uint256 _amount) external override onyAnts {
     _mint(_to, _amount);
+  }
+
+  function burn(address eggOwner, uint256 eggsToBurn) external override onyAnts {
+    // only an ant can be created with a single egg per time
+    _burn(eggOwner, eggsToBurn);
   }
 
   function decimals() public view virtual override returns (uint8) {
