3a2b7bb199ef38bdb6a5a66144bed13cb3b3c767
NicoBarragan <nbarragan01@gmail.com>
2022-09-01 18:20:13 -0300

    refactor(vrf-mock.ts): updated vrf vars and func return

--- test/utils/vrf-mock.ts
+++ test/utils/vrf-mock.ts
@@ -4,15 +4,16 @@ import { VRFCoordinatorV2Mock, VRFCoordinatorV2Mock__factory } from '@typechaine
 const logger = require('pino')();
 
 // define constants
-const CALLBACK_GAS_LIMIT = BigNumber.from('500000');
-const KEY_HASH = '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc';
+const CALLBACK_GAS_LIMIT = BigNumber.from('2500000');
+const KEY_HASH = '0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15';
 
 // const for deploying the vrf2 coordinator
 const BASE_FEE = ethers.utils.parseEther('0.25'); // 0.25 is the premium. It costs 0.25 LINK per request
 const GAS_PRICE_LINK = 1e9;
+let subscriptionId: BigNumber;
 
 // script for deploy
-const deployVRFv2Mock = async () => {
+const deployVRFv2Mock = async (): Promise<VRFCoordinatorV2Mock> => {
   if (network.name !== ('hardhat' || 'localhost')) {
     logger.warn('This script is for a local network only');
     process.exitCode = 1;
@@ -24,22 +25,21 @@ const deployVRFv2Mock = async () => {
   await vrfCoordinatorV2contract.deployed();
 
   // create subscription
-  let subscriptionId;
   const tx = await vrfCoordinatorV2contract.createSubscription();
   const txReceipt = await tx.wait(1); // This receives the events emitted in the tx
 
   const events = txReceipt.events;
   if (events && events[0].args) {
     subscriptionId = events[0].args.subId;
+    logger.info(`subscriptionId: ${subscriptionId}`);
   }
 
   // Fund subscription
-  const subFundAmount = ethers.utils.parseEther('2');
+  const subFundAmount = ethers.utils.parseEther('2000');
   await vrfCoordinatorV2contract.fundSubscription(subscriptionId, subFundAmount);
 
-  const vrfMockAddress = vrfCoordinatorV2contract.address;
-  return { vrfMockAddress, subscriptionId };
+  return vrfCoordinatorV2contract;
 };
 
 // for the e2e test in a forked chain purposals
-export { deployVRFv2Mock, KEY_HASH, CALLBACK_GAS_LIMIT };
+export { deployVRFv2Mock, KEY_HASH, CALLBACK_GAS_LIMIT, subscriptionId };
