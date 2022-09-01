56c1854961ce37900a5280c45c10ca3259f3f0c3
NicoBarragan <nbarragan01@gmail.com>
2022-09-01 18:17:41 -0300

    feat(Egg.sol): removed OnlyAnts modifier, made it Ownable and updated funcs modifier

--- contracts/Egg.sol
+++ contracts/Egg.sol
@@ -2,30 +2,23 @@
 pragma solidity >=0.8.4 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import './interfaces/IEgg.sol';
 
-contract Egg is ERC20, IEgg {
-  address private _ants;
-
-  modifier onlyAnts() {
-    if (msg.sender != _ants) revert OnlyAnts(_ants, msg.sender);
-    _;
-  }
+contract Egg is Ownable, ERC20, IEgg {
+  // solhint-disable-next-line
+  constructor() ERC20('EGG', 'EGG') {}
 
   modifier notZeroAddress(address tokenAddr) {
     if (tokenAddr == address(0)) revert NoZeroAddress();
     _;
   }
 
-  constructor(address __ants) ERC20('EGG', 'EGG') notZeroAddress(__ants) {
-    _ants = __ants;
-  }
-
-  function mint(address _to, uint256 _amount) external override onlyAnts {
+  function mint(address _to, uint256 _amount) external override onlyOwner {
     _mint(_to, _amount);
   }
 
-  function burn(address _from, uint256 _amount) external override onlyAnts {
+  function burn(address _from, uint256 _amount) external override onlyOwner {
     // only an ant can be created with a single egg per time
     _burn(_from, _amount);
   }
