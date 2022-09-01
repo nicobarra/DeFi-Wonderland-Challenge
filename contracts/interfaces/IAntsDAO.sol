b2ebabe54bf085d5de8b6ad781f2f152fe0039b9
NicoBarragan <nbarragan01@gmail.com>
2022-09-01 18:08:14 -0300

    refactor(IAntsDAO.sol): removed struct and enum from interface

--- contracts/interfaces/IAntsDAO.sol
+++ contracts/interfaces/IAntsDAO.sol
@@ -3,19 +3,7 @@ pragma solidity >=0.8.4 <0.9.0;
 
 interface IAntsDAO {
   // events
-  event ExecutedProposal(bool approved, uint256 proposalPrice, ProposalInfo info);
-
-  enum Status {
-    Pending,
-    Approved
-  }
-
-  struct ProposalInfo {
-    uint256 id;
-    uint256 timestamp;
-    Status status;
-    uint256 votes;
-  }
+  event ExecutedProposal(bool approved, uint256 proposalPrice);
 
   // external functions
   function proposeEggPrice(uint256) external;
@@ -27,6 +15,6 @@ interface IAntsDAO {
   // errors
   error NotVotingPower();
   error PriceAlreadyExists();
-  error ClosedProposal();
+  error ProposalNotFound();
   error UnfinishedPeriod();
 }
